30e1d089e1dcd36dfb026dd6e0b53802cbfb09a1
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-02-20 00:59:51 -0500

    Update auth component HTML and TypeScript files

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
@@ -15,4 +15,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AuthComponent { }
+export default class AuthComponent {
+
+  type = signal('password')
+
+  toggleType() {
+    console.log(this.type())
+    let inputPwd = document.getElementById('password') as HTMLInputElement;
+    if (inputPwd) {
+      if (inputPwd.type === 'password') {
+        inputPwd.type = 'text';
+        this.type.set('text');
+      } else {
+        this.type.set('password');
+        inputPwd.type = 'password';
+      }
+    } else {
+      console.log('No se encontró el elemento con id "password"');
+    }
+  }
+
+}
