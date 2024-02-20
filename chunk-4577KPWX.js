115848ef9f514d5ba8d05df226ec978b6fc0458e
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-02-19 21:14:17 -0500

    Auto-generated commit

---
+++ chunk-4577KPWX.js
@@ -0,0 +1,2 @@
+import{Aa as s,N as a,Pa as d,va as i,wa as r,za as o}from"./chunk-NULVXR77.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["app-board"]],standalone:!0,features:[s],decls:3,vars:0,template:function(n,p){n&1&&(i(0,"p"),o(1,"board works!"),r(),o(2,`,
+`))},dependencies:[d],styles:["[_nghost-%COMP%]{display:block}"],changeDetection:0});let t=e;return t})();export{f as default};
---
+++ chunk-HKIT5BUU.js
@@ -0,0 +1,2 @@
+import{Aa as s,N as a,Pa as c,va as i,wa as r,za as o}from"./chunk-NULVXR77.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["app-auth"]],standalone:!0,features:[s],decls:3,vars:0,template:function(n,d){n&1&&(i(0,"p"),o(1,"auth works!"),r(),o(2,`,
+`))},dependencies:[c],styles:["[_nghost-%COMP%]{display:block}"],changeDetection:0});let t=e;return t})();export{f as default};
