4b417f919036bef52975457af09baadd9a55b4cd
Esteban <112998820+Estebanbss@users.noreply.github.com.>
2024-03-08 15:30:28 -0500

    Auto-generated commit

--- chunk-VN37LG5A.js
+++
@@ -1,2 +0,0 @@
-import{Aa as s,N as a,Pa as c,va as i,wa as r,za as o}from"./chunk-NULVXR77.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["app-card"]],standalone:!0,features:[s],decls:3,vars:0,template:function(n,p){n&1&&(i(0,"p"),o(1,"card works!"),r(),o(2,`,
-`))},dependencies:[c],styles:["[_nghost-%COMP%]{display:block}"],changeDetection:0});let t=e;return t})();export{f as default};
